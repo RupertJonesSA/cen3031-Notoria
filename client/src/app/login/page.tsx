17189feb65deab7a2297257aea6b929c37d61fc3
RupertJonesSA <aljamal.sami05@gmail.com>
2024-10-21 16:45:52 -0400

    Optimized 3D renders for landing and home page, reducing loading time

--- client/src/app/login/page.tsx
+++ client/src/app/login/page.tsx
@@ -1,59 +1,89 @@
-'use client'
+"use client";
 
-import Spline from '@splinetool/react-spline/next';
-import { useState } from 'react';
+import Spline from "@splinetool/react-spline";
+import { useState } from "react";
+import Head from "next/head";
+import { Suspense } from "react";
 
-const Page = () =>{
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  
-  const handleSubmit = (e:any) => {
+const Page = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e: any) => {
     e.preventDefault();
-  } 
+  };
 
-  return(
-    <div className="bg-white w-screen h-screen">
-      <Spline
-        className="w-screen h-screen absolute"
-        scene="https://prod.spline.design/oKwB-1S0SrIuN3cg/scene.splinecode" 
-      />
+  return (
+    <>
+      <Head>
+        <link
+          rel="preconnect"
+          href="https://prod.spline.design"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="https://draft.spline.design/T7tCI0NsyUBMwcXM/scene.splinecode"
+          as="fetch"
+          crossOrigin="anonymous"
+        />
+      </Head>
+      <div className="bg-black w-screen h-screen">
+        <Spline
+          className="w-screen h-screen fixed"
+          scene="https://draft.spline.design/T7tCI0NsyUBMwcXM/scene.splinecode"
+        />
 
-      <form onSubmit={handleSubmit} className="absolute top-64 left-[690px] max-w-md mx-auto p-4">
-        <div className="mb-4">
-          <label htmlFor="email" className="block text-sm font-medium text-white font-custom">Email:</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-            className="mt-1 p-2 block w-fill border border-gray-300 rounded-md shadow-sm"
-          />
-        </div>
-
-        <div className="mb-4">
-          <label htmlFor="password" className="block text-sm font-medium text-white font-custom">Password:</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-            className= "mt-1 p-2 block w-full border border-gray-300 rounded-md shadow-sm"
-          />
-        </div>
-
-        <button
-          type="submit"
-          className="w-full bg-violet-950 text-white p-2 rounded-md hover:bg-purple-300 font-custom" 
+        <form
+          onSubmit={handleSubmit}
+          className="fixed top-48 left-[525px] max-w-md mx-auto p-4"
         >
-          Submit
-        </button>
-      </form>
-    </div>
+          <div className="mb-4">
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-white font-custom"
+            >
+              Email:
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              className="mt-1 p-2 block w-fill border border-gray-300 rounded-md shadow-sm text-black font-custom"
+            />
+          </div>
+
+          <div className="mb-4">
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-white font-custom"
+            >
+              Password:
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              className="mt-1 p-2 block w-full border border-gray-300 rounded-md shadow-sm text-black font-custom"
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="w-full bg-violet-950 text-white p-2 rounded-md hover:bg-purple-300 font-custom"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    </>
   );
-}
+};
 
 export default Page;
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,24 +1,46 @@
-'use client'
+"use client";
 
 import Spline from "@splinetool/react-spline";
-import { useRouter } from 'next/navigation';
+import { useRouter } from "next/navigation";
+import Head from "next/head";
+import { Suspense } from "react";
 
 const Page = () => {
   const router = useRouter();
-  const handleNavigation = () =>{
-    router.push('./login');
-  }
+  const handleNavigation = () => {
+    router.push("./login");
+  };
 
   return (
-    <div className="bg-white min-h-screen">
-      <Spline
-        className="absolute w-screen h-screen"
-        scene="https://prod.spline.design/RMkBnRtpqsLeb0oO/scene.splinecode"
-      />
-      <button onClick ={handleNavigation} className="w-64 h-14 bg-white absolute right-72 bottom-52 rounded-lg">
-        <h1 className="text-black font-custom text-xl">Login/Sign Up</h1>
-      </button>
-    </div>
+    <>
+      <Head>
+        <link
+          rel="preconnect"
+          href="https://prod.spline.design"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="https://draft.spline.design/5UoBCFUOm4z19NTS/scene.splinecode"
+          as="fetch"
+          crossOrigin="anonymous"
+        />
+      </Head>
+      <div className="bg-black min-h-screen">
+        <Suspense fallback={<div>Loading...</div>}>
+          <Spline
+            className="w-screen h-screen fixed"
+            scene="https://draft.spline.design/5UoBCFUOm4z19NTS/scene.splinecode"
+          />
+        </Suspense>
+        <button
+          onClick={handleNavigation}
+          className="w-64 h-14 bg-white fixed right-12 bottom-32 rounded-lg"
+        >
+          <h1 className="text-black font-custom text-xl">Get Started</h1>
+        </button>
+      </div>
+    </>
   );
 };
 
